31e62686074218cb56a1db716a6c9800853854c9
Jakub Holovsky <jakub.holovsky@iosbits.co.uk>
2017-05-06 12:46:55 +0200

    Splash logic updated.

--- ParkAndRidePrague/ParkAndRidePrague.Android/Resources/Resource.Designer.cs
+++ ParkAndRidePrague/ParkAndRidePrague.Android/Resources/Resource.Designer.cs
@@ -2339,26 +2339,26 @@ namespace ParkAndRidePrague.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f020112
+			public const int avd_hide_password_1 = 2130837778;
+			
 			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_1 = 2130837779;
+			public const int avd_hide_password_2 = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_2 = 2130837780;
-			
-			// aapt resource value: 0x7f020115
-			public const int avd_hide_password_3 = 2130837781;
+			public const int avd_hide_password_3 = 2130837780;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
+			// aapt resource value: 0x7f020115
+			public const int avd_show_password_1 = 2130837781;
+			
 			// aapt resource value: 0x7f020116
-			public const int avd_show_password_1 = 2130837782;
+			public const int avd_show_password_2 = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_2 = 2130837783;
-			
-			// aapt resource value: 0x7f020118
-			public const int avd_show_password_3 = 2130837784;
+			public const int avd_show_password_3 = 2130837783;
 			
 			// aapt resource value: 0x7f020055
 			public const int back_arrow = 2130837589;
@@ -2915,20 +2915,17 @@ namespace ParkAndRidePrague.Droid
 			// aapt resource value: 0x7f02010d
 			public const int notification_icon_background = 2130837773;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_bg = 2130837777;
+			// aapt resource value: 0x7f020110
+			public const int notification_template_icon_bg = 2130837776;
 			
-			// aapt resource value: 0x7f020112
-			public const int notification_template_icon_low_bg = 2130837778;
+			// aapt resource value: 0x7f020111
+			public const int notification_template_icon_low_bg = 2130837777;
 			
 			// aapt resource value: 0x7f02010e
 			public const int notification_tile_bg = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
 			public const int notify_panel_notification_icon_bg = 2130837775;
-			
-			// aapt resource value: 0x7f020110
-			public const int splash = 2130837776;
 			
 			static Drawable()
 			{
@@ -3862,13 +3859,16 @@ namespace ParkAndRidePrague.Droid
 			public const int select_dialog_singlechoice_material = 2130903110;
 			
 			// aapt resource value: 0x7f030047
-			public const int support_simple_spinner_dropdown_item = 2130903111;
+			public const int Splash = 2130903111;
 			
 			// aapt resource value: 0x7f030048
-			public const int Tabbar = 2130903112;
+			public const int support_simple_spinner_dropdown_item = 2130903112;
 			
 			// aapt resource value: 0x7f030049
-			public const int Toolbar = 2130903113;
+			public const int Tabbar = 2130903113;
+			
+			// aapt resource value: 0x7f03004a
+			public const int Toolbar = 2130903114;
 			
 			static Layout()
 			{
@@ -4906,9 +4906,6 @@ namespace ParkAndRidePrague.Droid
 			
 			// aapt resource value: 0x7f0b004d
 			public const int RtlUnderlay_Widget_AppCompat_ActionButton_Overflow = 2131427405;
-			
-			// aapt resource value: 0x7f0b018f
-			public const int SplashTheme = 2131427727;
 			
 			// aapt resource value: 0x7f0b00dd
 			public const int TextAppearance_AppCompat = 2131427549;
--- ParkAndRidePrague/ParkAndRidePrague.Android/SplashActivity.cs
+++ ParkAndRidePrague/ParkAndRidePrague.Android/SplashActivity.cs
@@ -5,15 +5,21 @@ using Android.Support.V7.App;
 
 namespace ParkAndRidePrague.Droid
 {
-    [Activity(Label = "Prague P+R", Icon = "@drawable/icon", Theme = "@style/SplashTheme", MainLauncher = true)]
+    [Activity(Label = "Prague P+R", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true)]
     public class SplashActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            var intent = new Intent(this, typeof(MainActivity));
-            StartActivity(intent);
-            Finish();
+            SetContentView(Resource.Layout.Splash);
+
+            var postDelayed = new Handler();
+            postDelayed.PostDelayed(() =>
+            {
+                var intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+                Finish();
+            }, 100);
         }
     }
 }
